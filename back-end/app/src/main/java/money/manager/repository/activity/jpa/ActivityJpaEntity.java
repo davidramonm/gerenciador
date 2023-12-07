6eab7a76dbcae0a4f70335951b32ac78871225c4
davidramonm <davidramonm@outlook.com>
2023-12-07 16:04:55 -0300

    Comecando a trabalhar com Springboot

---
+++ back-end/app/src/main/java/money/manager/repository/activity/jpa/ActivityJpaEntity.java
@@ -0,0 +1,10 @@
+package money.manager.repository.activity.jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity (name = "Activity")
+@Table (name = "activities")
+public class ActivityJpaEntity {
+    
+}
