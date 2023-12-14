1fb48254fccbbae694c0b85b7e94691edfcea47a
davidramonm <davidramonm@outlook.com>
2023-12-14 15:32:33 -0300

    comecando a utilizar docker

--- back-end/app/src/main/java/money/manager/repository/activity/ActivityJpaGateway.java
+++ back-end/app/src/main/java/money/manager/repository/activity/ActivityJpaGateway.java
@@ -1,28 +1,58 @@
 package money.manager.repository.activity;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.dao.OptimisticLockingFailureException;
+
+import jakarta.persistence.PersistenceException;
 import money.manager.domain.activity.Activity;
 import money.manager.domain.gateway.ActivityGateway;
+import money.manager.repository.activity.jpa.ActivityJpaEntity;
+import money.manager.repository.activity.jpa.ActivityJpaRepository;
 
 public class ActivityJpaGateway implements ActivityGateway {
 
-    @Override
-    public void create(Activity anActivity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    private ActivityJpaRepository activityJpaRepository;
+
+    private ActivityJpaGateway(final ActivityJpaRepository aRepository) {
+        this.activityJpaRepository = aRepository;
+    }
+
+    public static ActivityGateway build(final ActivityJpaRepository aRepository) {
+        return new ActivityJpaGateway(aRepository);
     }
 
     @Override
-    public void delete(String anId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    public void create(final Activity anActivity) {
+        final var anActivityEntity = ActivityJpaEntity.from(anActivity);
+
+        try {
+            this.activityJpaRepository.save(anActivityEntity);
+        } catch (IllegalArgumentException e) {
+            throw new PersistenceException(e.getMessage());
+        } catch (OptimisticLockingFailureException e) {
+            throw new PersistenceException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void delete(final String anId) {
+        try {
+            this.activityJpaRepository.deleteById(anId);
+
+        } catch (IllegalArgumentException e) {
+            throw new PersistenceException(e.getMessage());
+        }
+
     }
 
     @Override
     public List<Activity> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        final var aList = this.activityJpaRepository.findAll();
+        return aList.stream()
+                .map(activityEntity -> activityEntity.toModel())
+                .collect(Collectors.toList());
     }
-    
+
 }
--- back-end/app/src/main/java/money/manager/repository/activity/jpa/ActivityJpaEntity.java
+++ back-end/app/src/main/java/money/manager/repository/activity/jpa/ActivityJpaEntity.java
@@ -1,10 +1,135 @@
 package money.manager.repository.activity.jpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import java.time.Instant;
 
-@Entity (name = "Activity")
-@Table (name = "activities")
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import money.manager.domain.activity.Activity;
+import money.manager.domain.activity.type.Type.ActivityType;
+
+@Entity(name = "Activity")
+@Table(name = "activities")
 public class ActivityJpaEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "date", nullable = false)
+    private Instant date;
+    @Column(name = "value", nullable = false)
+    private Float value;
+    @Column(name = "type", nullable = false)
+    private Integer type;
+    @Column(name = "createdAt", nullable = false)
+    private Instant createdAt;
+    @Column(name = "updatedAt", nullable = false)
+    private Instant updatedAt;
     
+
+    public ActivityJpaEntity() {
+
+    }
+
+    private ActivityJpaEntity(final String anId, final String aDescription, final Instant aDate,
+            final Float aValue, final Integer aType, final Instant aCreatedAt, final Instant anUpdatedAt) {
+
+        this.id = anId;
+        this.description = aDescription;
+        this.date = aDate;
+        this.value = aValue;
+        this.type = aType;
+        this.createdAt = aCreatedAt;
+        this.updatedAt = anUpdatedAt;
+    }
+
+    public static ActivityJpaEntity from(final Activity anActivity) {
+
+        Integer aType;
+
+        if (anActivity.getType() == ActivityType.EXPENSE) {
+            aType = 0;
+        } else {
+            aType = -1;
+        }
+
+        return new ActivityJpaEntity(
+                anActivity.getId(),
+                anActivity.getDescription(),
+                anActivity.getDate(), anActivity.getValue(),
+                aType,
+                anActivity.getCreatedAt(),
+                anActivity.getUpdatedAt());
+    }
+
+    public Activity toModel(){
+        return Activity.with(
+            this.getId(),
+            this.getDate(),
+            this.getDescription(),
+            this.getValue(),
+            ActivityType.values()[this.getType()],
+            this.getCreatedAt(),
+            this.getUpdatedAt());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
